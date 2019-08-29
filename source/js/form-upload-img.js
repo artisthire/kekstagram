4954f6c0c0facc86e4f10d587d8bdbc709fd5e44
Andrey <artist_hire@ukr.net>
2019-08-29 11:28:17 +0300

    Изменил функционал формы загрузки изображения, чтобы он использовал один модуль для изменения эффектов на загружаемом изображении

--- source/js/form-upload-img.js
+++ source/js/form-upload-img.js
@@ -1,73 +1,82 @@
+/**
+ * Модуль формы загрузки изображения и наложения на него эффектов
+ * Использует функционал ChangeImgEffect для масштабирования изображения и наложения на него художественных эффектов
+ * Функционал Popup используется, так как форма отображается в виде попапа
+ * @module ./form-upload-img
+ */
 import {ChangeImgEffect} from './change-img-effect.js';
-import {BtnsRangeSwitch} from './btns-range-switch.js';
 import {Popup} from './popup.js';
 
+/**
+ * Class представляет собой объект формы для загрузки и наложения эффектов на изображение
+ */
 export class FormUploadImg {
-  // TODO: Добавить обработчки изменения эффектов, наложенных на изобржение
   _popup;
-  _scaler;
   _changerImgEffect;
 
+  /**
+   * Создает объект формы
+   * @param {object} btnUploadFile - HTML-элемент - ссылка на кнопку загрузки изображения
+   */
   constructor(btnUploadFile) {
     this.btnUploadFile = btnUploadFile;
 
+    // элементы модального окна
     this._modalOverlay = document.querySelector('.img-upload__overlay');
     this._modalContainer = this._modalOverlay.querySelector('.img-upload__wrapper');
     this._modalCloseBtn = this._modalContainer.querySelector('#upload-cancel');
 
+    // поля формы
     this._hashtagInput = this._modalContainer.querySelector('.text__hashtags');
     this._descriptionInput = this._modalContainer.querySelector('.text__description');
     this._imgElement = this._modalContainer.querySelector('.img-upload__preview > img');
 
+    // добавляет обработчки загрузки файла на переданную кнопку управления
     this._onBtnUploadFileChange = this._onBtnUploadFileChange.bind(this);
     this.btnUploadFile.addEventListener('change', this._onBtnUploadFileChange);
   }
 
-
+  /**
+   * Создает форму загрузки изображения при выборе изображения для загрузки
+   * Форма создается в отдельно попапе
+   */
   _onBtnUploadFileChange() {
     this._popup = new Popup(this._modalOverlay, this._modalContainer, [this._hashtagInput, this._descriptionInput], this._modalCloseBtn, 'hidden', 'modal-open');
 
+    // добавляем обработку закрытия попапа с формой
     this._destroyForm = this._destroyForm.bind(this);
     this._popup.addClosePopupListener(this._destroyForm);
+
+    // отображаем форму
     this._popup.showPopup(this._initForm.bind(this));
   }
 
+  /**
+   * Функция начальной инициализации формы при отображении
+   */
   _initForm() {
-    let rangeSwitchContainer = document.querySelector('.scale');
-    this._scaler = new BtnsRangeSwitch(
-        {container: rangeSwitchContainer},
-        {startValue: 100, valueStep: 25, minValue: 25, maxValue: 100});
-
-    this._scaleImg = this._scaleImg.bind(this);
-    this._scaler.addChangeListener(this._scaleImg);
-
+    // добавляем обработчик изменения масштаба и эффектов загружаемого изображения
     this._changerImgEffect = new ChangeImgEffect(this._imgElement);
   }
 
+  /**
+   * Функция сброса формы
+   * Вызывается автоматически при закрытии попапа внутри которой находится форма
+   */
   _destroyForm() {
-    this._scaler.removeChangeListener(this._scaleImg);
-    this._scaler.destructor();
-    this._scaler = null;
-
+    // удаляем попап
     this._popup.removeClosePopupListener(this._destroyForm);
     this._popup = null;
 
+    // удаляем обработчик эффектов изображения
     this._changerImgEffect.destructor();
     this._changerImgEffect = null;
 
     // сбрасываем значение input[type='file'] для повторной сработки события 'change' если изображение будет тем же
     this.btnUploadFile.value = '';
 
-    // сбрасываем содержимое полей хэштегов и описания изображения
+    // сбрасываем содержимое тектовых полей
     this._hashtagInput.value = '';
     this._descriptionInput.value = '';
-
-    // сбрасываем примененные к изображению стили
-    this._imgElement.style.transform = '';
-  }
-
-  _scaleImg(evt) {
-    let scaleValue = evt.detail.value / 100;
-    this._imgElement.style.transform = `scale(${scaleValue})`;
   }
 }
