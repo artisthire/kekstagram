346cdc818816f269aadf0c250f493226985b8255
Andrey <artist_hire@ukr.net>
2019-10-11 11:07:53 +0300

    Закончил функционал для окна отображени выбранной картинки

--- source/js/picture-preview.js
+++ source/js/picture-preview.js
@@ -1,17 +1,26 @@
 import {Popup} from './popup.js';
 
+/**
+ * Модуль функционала полноэкранного просмотра выбранной пользовательской картинки
+ * Большая картинка отбражается при клике на одну из ссылок в отдельном модально попапе
+ * @module ./popup
+ */
+
 export class PicturePreview {
 
-  constructor(linksPictureContainer) {
-    // контейнер с ссылками на картинки
-    this.linksContainer = linksPictureContainer;
+  /**
+   * Создает объект модуля полноэкранного просмотра картинки
+   * @param {object} linksPicturesContainer - контейнер, содержащий все ссылки с миниатюрами картинок пользователей
+   */
+  constructor(linksPicturesContainer) {
+    this.linksContainer = linksPicturesContainer;
 
     // элементы модального окна
     this._modalOverlay = document.querySelector('.big-picture');
     this._modalContainer = this._modalOverlay.querySelector('.big-picture__preview');
     this._modalCloseBtn = this._modalContainer.querySelector('#picture-cancel');
 
-    // элементы для отображения данных по картинке
+    // элементы для отображения данных по картинке внутри попапа
     this._imgElement = this._modalContainer.querySelector('.big-picture__img > img');
     this._imgTitle = this._modalContainer.querySelector('.social__caption');
     this._likesCount = this._modalContainer.querySelector('.likes-count');
@@ -24,6 +33,10 @@ export class PicturePreview {
     this.linksContainer.addEventListener('click', this._onImgLinkClick);
   }
 
+  /**
+   * Обработчик клика на одну из ссылок, содержащих миниатюры картинок
+   * @param {object} evt - объект события клика
+   */
   _onImgLinkClick(evt) {
     // получаем элемент ссылки на которой был клик
     let imgLink = evt.target.closest('.picture');
@@ -44,22 +57,20 @@ export class PicturePreview {
     // находим данные для выбранной картинки
     // поочередно сравнивая ссылки на картинки в загруженных данных с ссылкой на картинку элемента по которому кликнули
     let pictureData = picturesData.find((currentPictureData) => currentPictureData.url === imgElement.getAttribute('src'));
-    console.log(pictureData);
     // создаем попап для отображения модального окна с предпросмотром выбранной картинки
     this._popup = new Popup(this._modalOverlay, this._modalContainer, [this._userCommentInput], this._modalCloseBtn, 'hidden', 'modal-open');
+
     // инициируем и отображаем попап с картинкой
     this._initPicturePreviewBlock(pictureData);
-
     // добавляем обработку события закрытия попапа с картинкой
     this._destroyPicturePreviewBlock = this._destroyPicturePreviewBlock.bind(this);
     this._popup.on(this._popup.EVENT_CLOSE_POPUP, this._destroyPicturePreviewBlock);
-
     // отображаем попап
     this._popup.showPopup();
   }
 
   /**
-   * Модифицирует блок отображающий выбранную картинку в полноэкранном режиме
+   * Загружает в попап данные по картинке и комментариям к ней
    * @param {object} pictureData - объект с данными по выбранной картинке
    */
   _initPicturePreviewBlock(pictureData) {
@@ -67,9 +78,13 @@ export class PicturePreview {
     this._imgTitle.textContent = pictureData.description;
     this._likesCount.textContent = pictureData.likes;
 
+    // создать отдельный блок с комментариями к выбранной картинке
     this._socialComments = new SocialCommentsBlock(pictureData.comments);
   }
 
+  /**
+   * Сбрасывает информацию о картинке и установленных обработчиках при закрытии попапа
+   */
   _destroyPicturePreviewBlock() {
     this._popup.off(this._popup.EVENT_CLOSE_POPUP, this._destroyPicturePreviewBlock);
     this._popup = null;
@@ -79,13 +94,18 @@ export class PicturePreview {
   }
 }
 
-
+/**
+ * Отдельный класс для блока комментарие пользователей к выбранной картинке
+ */
 class SocialCommentsBlock {
 
   // используются для порционного отображения комментариев при клика на кнопку загрузки большего колличества комментариев
-  _nextCommentItem = 0;
   COMMENTS_SHOW_STEP = 5;
 
+  /**
+   * Создает объект блока с комментариями к выбранной картинке
+   * @param {object} commentsData - объект с частичными данными по выбранной картинке, содежит только то что касается комментариев
+   */
   constructor(commentsData) {
     this.commentsData = commentsData;
     this._container = document.querySelector('.social');
@@ -97,11 +117,17 @@ class SocialCommentsBlock {
     this._initSocialComments();
   }
 
+  /**
+   * Сбрасывает информацию о комментариях к картинке при закрытии попапа
+   */
   destroySocialCommentsBlock() {
     this._userCommentInput.value = '';
     this._btnMoreComments.removeEventListener('click', this._onMoreCommentsClick);
   }
 
+  /**
+   * Инициирует блок с комментариями
+   */
   _initSocialComments() {
     this._nextCommentItem = 0;
     this._btnMoreComments.disabled = false;
@@ -118,7 +144,8 @@ class SocialCommentsBlock {
   }
 
   /**
-   * Добавляет социальные комментарии, модифицирует информацию об их количестве
+   * Согласно COMMENTS_SHOW_STEP по частям показывает комментарии пользователей к картинке
+   * Колличество отображаемых комментариев увеличивается при нажатии соответствующей кнопки
    */
   _updateSocialComments() {
     // сохраняем стартовое значение и увеличиваем диапазон комментариев для отображения
@@ -142,32 +169,30 @@ class SocialCommentsBlock {
   }
 
   /**
-   * Добавляет в контейнер блок комментариев, данные по которым передаются через массив
-   *
+   * Добавляет в общий контейнер элементы с комментариеми пользователей
    * @param {array} commentsData - массив данных по коментариям, которые нужно добавить в общий контейнер
    */
   _addSocialComments(commentsData) {
     // создаем и заполняем массив элементов LI для списка комментариев
-    let socialCommentElements = commentsData.map((commentData) => this._createSocialCommentElement(commentData));
+    let socialCommentElements = commentsData.map((userComment) => this._createSocialCommentElement(userComment));
     // добавляем комментарии в общий контейнер
     this._commentsList.append(...socialCommentElements);
   }
 
   /**
-   * Создает HTML-элемент LI содержащий разметку для каждого отдельного комментария под фотографией
-   *
-   * @param {string} commentData - Текст комментария
-   * @return {object} - элемент <LI>, содержащий разметку для вставки коментариев в контейнер
+   * Создает HTML-элемент, содержащий отдельный комментарий конкретного пользователя
+   * @param {string} userComment - данные о комментарии и пользователе, который его добавил
+   * @return {object} - HTML-элемент, содержащий разметку для вставки в общий контейнер
    */
-  _createSocialCommentElement(commentData) {
+  _createSocialCommentElement(userComment) {
 
     let commentItem = document.createElement('li');
     commentItem.className = 'social__comment';
 
-    commentItem.innerHTML = `<img class="social__picture" src="${commentData.avatar}"
+    commentItem.innerHTML = `<img class="social__picture" src="${userComment.avatar}"
       alt="Аватар комментатора фотографии" width="35" height="35">
-      <p class="social__text">${commentData.message}<br>
-      <b>${commentData.name}</b></p>`;
+      <p class="social__text">${userComment.message}<br>
+      <b>${userComment.name}</b></p>`;
 
     return commentItem;
   }
