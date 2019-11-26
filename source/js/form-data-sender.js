43cdd1f07e1145c37cc839ff15e90d83adddbb5f
Andrey <artist_hire@ukr.net>
2019-11-26 15:33:33 +0200

    Добавил комментарии в файл отправки данных формы загруки изображения на сервер

--- source/js/form-data-sender.js
+++ source/js/form-data-sender.js
@@ -9,14 +9,23 @@ import {Popup} from './popup.js';
 
 export class FormDataSender {
 
+  /**
+   * Создает объект для функционала отправки данных формой загрузки изображений
+   * @param {object} form - форма, данные с которой отправлются на сервер
+   * @param {object} popup - ссылка на окно (попап), в котором содержится форма
+   * @param {object} showFormBtn - ссылка на кнопку, с помощью которой вызывается отрытие формы загрузки изображения
+   */
   constructor(form, popup, showFormBtn) {
     this.form = form;
     this.formPopup = popup;
     this.showFormBtn = showFormBtn;
     this._formData = new FormData(this.form);
 
+    // окно, которое отображается во время загрузи изображения на сервер
     this._porgressWindow = document.querySelector('#messages').content.querySelector('.overlay').cloneNode(true);
+    // окно, которое отображается при успешной загрузке изображения на сервер
     this._successWindow = document.querySelector('#success').content.querySelector('.success').cloneNode(true);
+    // окно, которое отображается в случае ошибок загрузки формы на сервер
     this._errorWindow = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
 
     this.sendData = this.sendData.bind(this);
@@ -24,13 +33,21 @@ export class FormDataSender {
     this._errorSend = this._errorSend.bind(this);
   }
 
+  /**
+   * Отправляет данные формы загрузки изображения на сервер
+   */
   sendData() {
     // добавляем окно прогресса отправки данных
     document.body.append(this._porgressWindow);
+    // отправляем данные
     sendDataPicture(this._formData, this._successSend, this._errorSend);
   }
 
+  /**
+   * Callback-функция, которая вызывается при успешной отправке данных формы
+   */
   _successSend() {
+    // закрываем форму загрузки изображения
     this._closeFormPopup();
 
     // показываем окно сообщения об успешной отправке данных
@@ -47,6 +64,9 @@ export class FormDataSender {
     successPopup.on(successPopup.EVENT_CLOSE_POPUP, () => this._successWindow.remove());
   }
 
+  /**
+   * Callback-функция, которая вызывается в случае ошибки отправки данных формы на сервер
+   */
   _errorSend() {
     this._closeFormPopup();
 
@@ -57,8 +77,11 @@ export class FormDataSender {
     let overlay = this._errorWindow;
     let container = this._errorWindow.firstElementChild;
     let inputFields = null;
+    // кнопка повторной попытки отправки данных на форму
     this._tryAgainBtn = this._errorWindow.querySelector('[data-try-again]');
+    // кнопка открытия формы для загрузки новой картинки для отправки на сервер
     this._loadNewFileBtn = this._errorWindow.querySelector('[data-load-new]');
+    // попап ошибки отправки закрывается при нажатии на любую из кнопок
     let closeBtns = [this._tryAgainBtn, this._loadNewFileBtn];
 
     this._onTryAgainBtnClick = this._onTryAgainBtnClick.bind(this);
@@ -72,27 +95,44 @@ export class FormDataSender {
     errorPopup.on(errorPopup.EVENT_CLOSE_POPUP, this._closeErrorWindow);
   }
 
+  /**
+   * Функция повторной отправки данных на сервер
+   * @param {object} evt - объект события клика на кнопке
+   */
   _onTryAgainBtnClick(evt) {
     evt.preventDefault();
+    // закрытие окна с ошибкой отправки
     this._closeErrorWindow();
-
+    // повторная попытка отправки данных на сервер
     this.sendData();
   }
 
+  /**
+   * Функция открытия новой формы для загрузки нового изображения
+   * @param {object} evt - объект события клика на кнопке
+   */
   _onLoadNewFileBtnClick(evt) {
     evt.preventDefault();
+    // закрытие окна с ошибкой отправки
     this._closeErrorWindow();
-
     // вызываем форму загрузки файла заново
     this.showFormBtn.click();
   }
 
+  /**
+   * Закрытие окна ошибки отправки изображения на сервер
+   */
   _closeErrorWindow() {
     this._tryAgainBtn.removeEventListener('click', this._onTryAgainBtnClick);
     this._loadNewFileBtn.removeEventListener('click', this._onLoadNewFileBtnClick);
     this._errorWindow.remove();
   }
 
+  /**
+   * Закрытие попапа формы загрузки изображения на сервер
+   * Форма закрывается при успешной и не успешной отправке данных на сервер
+   * Также функция закрывает окно прогресса отправки изображения
+   */
   _closeFormPopup() {
     // убираем окно прогресса отправки данных
     this._porgressWindow.remove();
